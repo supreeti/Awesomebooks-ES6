af1c9eb2865c423cd397e0ac0a45b9be5742add8
Supreeti <sksupreetk11@gmail.com>
2023-03-02 18:44:24 +0530

    resolve linter errors

--- modules/classBook.js
+++ modules/classBook.js
@@ -1,20 +1,20 @@
 class AwesomeBook {
-    constructor() {
-      this.bookList = JSON.parse(localStorage.getItem('storedData')) || [];
-    }
-  
-    addBookAwes(title, author) {
-      this.bookList.push({ title, author });
-      this.updateLocal();
-    }
-  
-    removeBookAwes(index) {
-      this.bookList.splice(index, 1);
-      this.updateLocal();
-    }
-  
-    updateLocal() {
-      localStorage.setItem('storedData', JSON.stringify(this.bookList));
-    }
+  constructor() {
+    this.bookList = JSON.parse(localStorage.getItem("storedData")) || [];
   }
-  export default AwesomeBook;
+
+  addBookAwes(title, author) {
+    this.bookList.push({ title, author });
+    this.updateLocal();
+  }
+
+  removeBookAwes(index) {
+    this.bookList.splice(index, 1);
+    this.updateLocal();
+  }
+
+  updateLocal() {
+    localStorage.setItem("storedData", JSON.stringify(this.bookList));
+  }
+}
+export default AwesomeBook;
